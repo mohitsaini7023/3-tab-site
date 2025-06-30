a1fd6452bcd5e04a9dc20823079594c7e1f5119e
Mohit Saini <mohitsaini80800@gmail.com>
2025-06-30 10:10:53 +0530

    dbnhd

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import Toy from './Toy'
 import Member from './Member'
 
 const App = () => {
-  const[tab,settab]=useState('tab3')
+  const[tab,settab]=useState('tab1')
   return (
     <div>
       
 <div className='bg-dark w-100 py-5'>
     <div className='container rounded-pill'>
       <div className='row'>
-        <div className='col-lg-4 col-md-6 col-sm-12 text-center py-2 div3'><div onClick={()=>settab('tab1')} className={tab=='tab1'?'active':'none'}><button className='w-100 rounded-pill py-3 btn3'>WEDDING</button></div></div>
+        <div className='col-lg-4 col-md-6 col-sm-12 text-center py-2 div3'><div onClick={()=>settab('tab1')} className={tab=='tab1'?'active':'none'}><button className='w-100 rounded-pill py-3 btn3'><span>WEDDING</span></button></div></div>
         <div className='col-lg-4 col-md-6 col-sm-12 text-center py-2 div3'><div onClick={()=>settab('tab2')} className={tab=='tab2'?'active':'none'}><button className='w-100 rounded-pill py-3 btn3'>TOY</button></div></div>
         <div className='col-lg-4 col-md-6 col-sm-12 text-center py-2 div3'><div onClick={()=>settab('tab3')} className={tab=='tab3'?'active':'none'}><button className='w-100 rounded-pill py-3 btn3'>MEMBER</button></div></div>
       </div>
